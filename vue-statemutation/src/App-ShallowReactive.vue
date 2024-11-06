a1c9c45ce0a705995ed1c2c6ad7473963837fd70
subugee <sasubramanian_md@hotmail.com>
2024-11-06 08:49:04 +0530

    reactive state

---
+++ vue-statemutation/src/App-ShallowReactive.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive,  shallowReactive } from "vue";
+// let review = reactive({
+//     like: 0,
+//     dislike: 0,
+//     recommend: {
+//         rating: {
+//             value: 0
+//         }
+//     }
+// })
+let review = shallowReactive({
+    like: 0,
+    dislike: 0,
+    recommend: {
+        rating: {
+            value: 0
+        }
+    }
+})
+
+const increment = () => {
+    review.like++
+}
+const decrement = () => {
+    review.dislike++
+}
+</script>
+<template>
+    <div>
+        <h1>Review Component</h1>
+        <h1>Like {{ review.like }} Dislike {{ review.dislike }}  Rating {{review.recommend.rating.value}}</h1>
+        <button @click="increment">Like</button>
+        <button @click="decrement">Dislike</button>
+        <button @click="review.recommend.rating.value++">Rating</button>
+    </div>
+</template>
+<style></style>
--- vue-statemutation/src/App.vue
+++ vue-statemutation/src/App.vue
@@ -1,15 +1,6 @@
 <script setup>
-import { reactive,  shallowReactive } from "vue";
-// let review = reactive({
-//     like: 0,
-//     dislike: 0,
-//     recommend: {
-//         rating: {
-//             value: 0
-//         }
-//     }
-// })
-let review = shallowReactive({
+import { reactive, toRefs } from "vue";
+let { like, dislike, recommend } = toRefs(reactive({
     like: 0,
     dislike: 0,
     recommend: {
@@ -17,22 +8,23 @@ let review = shallowReactive({
             value: 0
         }
     }
-})
+}))
+
 
 const increment = () => {
-    review.like++
+    like.value++
 }
 const decrement = () => {
-    review.dislike++
+    dislike.value++
 }
 </script>
 <template>
     <div>
         <h1>Review Component</h1>
-        <h1>Like {{ review.like }} Dislike {{ review.dislike }}  Rating {{review.recommend.rating.value}}</h1>
+        <h1>Like {{ like }} Dislike {{ dislike }} Rating {{ recommend.rating.value }}</h1>
         <button @click="increment">Like</button>
         <button @click="decrement">Dislike</button>
-        <button @click="review.recommend.rating.value++">Rating</button>
+        <button @click="recommend.rating.value++">Rating</button>
     </div>
 </template>
 <style></style>
