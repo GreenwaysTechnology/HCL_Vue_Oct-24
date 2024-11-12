671614fc65e5b71160c6dc41644b1f75306e2faf
subugee <sasubramanian_md@hotmail.com>
2024-11-12 14:47:43 +0530

    custom directives

---
+++ vue-customdirectives/src/App-simpleDirective.vue
@@ -0,0 +1,14 @@
+<script setup>
+//first directive
+const vFocus = {
+  //define life cycle method
+  mounted: (element) => {
+    console.log('vFocus directive')
+    console.log(element)
+    element.focus()
+  },
+}
+</script>
+<template>
+  <input v-focus />
+</template>
---
+++ vue-customdirectives/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const VHighlight = {
+    mounted: (element) => {
+        //add event listener
+        element.addEventListener('mousemove', (evt) => {
+            //  element.style.backgroundColor = 'yellow'
+            highLight('yellow')
+        })
+        element.addEventListener('mouseleave', (evt) => {
+            //element.style.backgroundColor = null
+            highLight(null)
+        })
+        const highLight = (color) => {
+            element.style.backgroundColor = color
+        }
+    }
+}
+</script>
+<template>
+    <h1 v-highlight>Touch Me</h1>
+</template>
